fe56fb5659f9d108f01daa1bc6a3ff53fcc914bd
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-03 19:52:52 -0700

    Test new expectations for searches index endpoint

--- spec/requests/api/v1/searches_spec.rb
+++ spec/requests/api/v1/searches_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
-describe 'GET /api/v1/searches' do
-  it 'returns searches with a valid request' do
-    get "/api/v1/searches"
+RSpec.describe 'GET /api/v1/searches', type: :request do
+  context 'when no data exists' do
+    it 'returns an empty array for valid requests' do
+      get "/api/v1/searches"
 
-    searches = JSON.parse(response.body)
+      searches = JSON.parse(response.body)
 
-    expect(response).to be_successful
+      expect(response).to be_successful
+      expect(searches["data"]).to eq([])
+    end
+  end
+
+  context 'when data exists' do
+    let!(:searches) { create_list(:search, 3)}
+
+    it 'returns searches for valid requests' do
+
+      get "/api/v1/searches"
+
+      searches = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(searches["data"][0]).to have_key("id")
+      expect(searches["data"][0]).to have_key("type")
+      expect(searches["data"][0]).to have_key("attributes")
+      expect(searches["data"][0]["attributes"]).to have_key("url")
+      expect(searches["data"][0]["attributes"]).to have_key("created_at")
+      expect(searches["data"][0]["attributes"]).to have_key("updated_at")
+    end
   end
 end
