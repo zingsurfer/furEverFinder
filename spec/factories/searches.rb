9a03b44821d2db34d9699578d53503b0c2223e4c
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-06 23:58:20 -0700

    Set response headers [ATP]

--- spec/factories/searches.rb
+++ spec/factories/searches.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :search do
-    url { Faker::Internet.url(host: 'https://www.mysterious-url.com', path: "/#{Faker::Superhero.power}")}
+    url { Faker::Internet.url(host: 'https://www.mysterious-url.com', path: "/#{Faker::Superhero.unique.power}")}
   end
 end
