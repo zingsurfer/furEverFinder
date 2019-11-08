87a4796ad152fcb5db31ed86086e668662d1d13e
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-07 20:59:15 -0700

    Plan for requests without params to visit dog pics endpoint

--- spec/requests/api/v1/dog_pics_controller_spec.rb
+++ spec/requests/api/v1/dog_pics_controller_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'GET /api/v1/dog_pics/random', type: :request do
   context 'valid request' do
-    it 'returns a pic and saves the search' do
+    it 'returns a pic and saves the search with params' do
       get '/api/v1/dog_pics/random?type=corgi'
+
+      expect(Search.all.length()).to eq(1)
+      expect(response).to be_successful
+      expect(response.header["Accept"]).to eq("application/json")
+      expect(response.header["Content-Type"]).to eq("application/json")
+    end
+    it 'returns a pic and saves the search without params' do
+      get '/api/v1/dog_pics/random'
 
       expect(Search.all.length()).to eq(1)
       expect(response).to be_successful
