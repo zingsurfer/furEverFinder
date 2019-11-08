3e6936742913aa10a574b543a5c159d485f1fdf7
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-07 20:52:26 -0700

    Lock down params & save dog pic searches

--- app/controllers/api/v1/dog_pics_controller.rb
+++ app/controllers/api/v1/dog_pics_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::DogPicsController < ApplicationController
   def show
-    find = DogPicFetcher.new(params[:type]).dog_pic
+    find = DogPicFetcher.new(dog_pic_params[:type]).dog_pic
 
     if find.image_url == nil
       render json: {error: "furEverFinder couldn't find a pic."}, status: 404
     else
+      search_url = request.original_url
+      Search.create(url: search_url)
+
       render json: find, status: 200
     end
+  end
+
+  def dog_pic_params
+    params.permit(:type)
   end
 end
