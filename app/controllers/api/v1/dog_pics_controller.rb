2f74a2ca9e025bd2400d29146bb29ff50fbbf796
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-10 20:50:17 -0700

    Add note for later

--- app/controllers/api/v1/dog_pics_controller.rb
+++ app/controllers/api/v1/dog_pics_controller.rb
@@ -3,8 +3,9 @@ class Api::V1::DogPicsController < ApplicationController
     find = DogPicFetcher.new(dog_pic_params[:type]).dog_pic
 
     if find.image_url == nil
-      render json: {error: "furEverFinder couldn't find a pic."}, status: 404
+      render json: { error: "furEverFinder couldn't find a pic." }, status: 404
     else
+      # TODO: Could move search saving to a job queue with Sidekiq
       search_url = request.original_url
       Search.create(url: search_url, topic: "dog_pics")
 
