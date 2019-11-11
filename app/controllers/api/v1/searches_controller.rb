d7c456ccb1f57a6cd7b1be8acc55d054bde75200
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-10 17:04:16 -0700

    Let the sorting begin! (& filtering) [ATP]

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,13 +1,55 @@
 class Api::V1::SearchesController < ApplicationController
-  def index
-    searches = search_params[:ordered_by].nil? ? Search.all : Search.ordered_by_creation(search_params[:ordered_by])
+  include ::ParamsHelper
+  rescue_from UnpermittedParamValue, with: :unpermitted_param_value_response
+  rescue_from UnsupportedParamCombo, with: :unsupported_param_combo_response
 
-    render json: Api::V1::SearchSerializer.new(searches).serializable_hash, status: 200
+  def index
+    render json: Api::V1::SearchSerializer.new(requested_searches).serializable_hash, status: 200
   end
 
   private
 
-  def search_params
-    params.permit(:ordered_by)
+  def search_index_params
+    params.permit(:id, :ordered_by, :sorted_by_topic, :topic)
+  end
+
+  def requested_searches
+    topic = search_index_params[:topic]
+    order_request = search_index_params[:ordered_by]
+    sort_request = search_index_params[:sorted_by_topic]
+
+    filtered_searches = filter_searches(topic)
+
+    organize_searches(filtered_searches, order_request, sort_request)
+  end
+
+  def filter_searches(topic)
+    if topic.nil?
+      Search.all
+    else
+      Search.topic_filtered(topic)
+    end
+  end
+
+  def organize_searches(searches, order_request, sort_request)
+    if sort_request.present? && order_request.present?
+      raise UnsupportedParamCombo.new(params:[ :ordered_by, :sorted_by_topic])
+    elsif sort_request.present?
+      if sort_request.downcase != "true"
+        raise UnpermittedParamValue.new(key: :sorted_by_topic, value: search_index_params[:sorted_by_topic])
+      end
+      searches.topic_sorted
+    elsif order_request.present?
+      validate_ordered_by_params
+      searches.creation_ordered(order_request)
+    else
+      searches
+    end
+  end
+
+  def validate_ordered_by_params
+    if !['newist_created', 'oldest_created'].include? search_index_params[:ordered_by]
+      raise UnpermittedParamValue.new(key: :ordered_by, value: search_index_params[:ordered_by])
+    end
   end
 end
