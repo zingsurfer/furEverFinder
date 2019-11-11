2d054e9aa68d6654934266198cca0ce7065e7f3e
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-11-10 17:44:03 -0700

    Add searches show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/searches', to: 'searches#index'
+      resources :searches, only: [:index, :show]
       get '/dog_pics/random', to: 'dog_pics#show'
     end
   end
